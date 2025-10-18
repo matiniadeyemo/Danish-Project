265a72a578216860100b0eaf8dcf3ca1ca58f1f5
matiniadeyemo <matiniadeyemo@gmail.com>
2025-10-18 14:00:55 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/components/home.vue'
 import contact from '@/components/contact.vue'
+import Vat from '@/components/Vat.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path:'/contact',name:'contact', component:contact
+    },
+    {
+      path:'/Vat',name:'Vat', component:Vat
     },
       { 
       path: '/', 
