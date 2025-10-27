add79a70acc3b6feb42407a5ca3d9bd11dd72001
matiniadeyemo <matiniadeyemo@gmail.com>
2025-10-27 07:14:07 -0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import home from '@/components/home.vue'
 import contact from '@/components/contact.vue'
 import Vat from '@/components/Vat.vue'
-
+import Travel from '@/components/Travel.vue';
+import Online from '@/components/Online.vue';
+import Gift from '@/components/Gift.vue';
+import moveDem from '@/components/moveDem.vue';
+import Special from '@/components/Special.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,6 +15,22 @@ const router = createRouter({
     },
     {
       path:'/Vat',name:'Vat', component:Vat
+    },
+     {
+      path:'/Travel',name:'Travel', component:Travel
+    },
+    {
+      path:'/Online', name:' Online', component: Online
+    }
+    ,
+    {
+      path:'/gift', name:' gift', component:Gift
+    },
+     {
+      path:'/move', name:' move', component:moveDem
+    },
+     {
+      path:'/Special', name:' Special', component:Special
     },
       { 
       path: '/', 
